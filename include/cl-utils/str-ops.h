9207258c2a5165ea566781175dfc668745d37870
Pete Cordell <pete.git@codalogic.com>
2016-09-22 12:25:34 +0100

    Added str-ops.h with string trim operations

---
+++ include/cl-utils/str-ops.h
@@ -0,0 +1,87 @@
+//----------------------------------------------------------------------------
+// Copyright (c) 2016, Codalogic Ltd (http://www.codalogic.com)
+// All rights reserved.
+//
+// The license for this file is based on the BSD-3-Clause license
+// (http://www.opensource.org/licenses/BSD-3-Clause).
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions
+// are met:
+//
+// - Redistributions of source code must retain the above copyright notice,
+//   this list of conditions and the following disclaimer.
+// - Redistributions in binary form must reproduce the above copyright notice,
+//   this list of conditions and the following disclaimer in the documentation
+//   and/or other materials provided with the distribution.
+// - Neither the name Codalogic Ltd nor the names of its contributors may be
+//   used to endorse or promote products derived from this software without
+//   specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+// ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+// LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+// CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+// SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+// INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+// CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+// POSSIBILITY OF SUCH DAMAGE.
+//----------------------------------------------------------------------------
+
+#ifndef CLUTIL_STR_OPS
+#define CLUTIL_STR_OPS
+
+#include <string>
+
+namespace clutils {
+
+const char * ws = " \t\n\r\f\v";
+
+// in-place trim from beginning of string
+inline std::string & ltrim( std::string * p_s, const char * p_what = ws )
+{
+    p_s->erase( 0, p_s->find_first_not_of( p_what ) );
+    return *p_s;
+}
+
+// copy trim from beginning of string
+inline std::string ltrim( const std::string & r_s, const char * p_what = ws )
+{
+    std::string s( r_s );
+    return ltrim( &s, p_what );
+}
+
+// in-place trim from end of string
+inline std::string & rtrim( std::string * p_s, const char * p_what = ws )
+{
+    p_s->erase( p_s->find_last_not_of( p_what ) + 1 );
+    return *p_s;
+}
+
+// copy trim from end of string
+inline std::string rtrim( const std::string & r_s, const char * p_what = ws )
+{
+    std::string s( r_s );
+    return rtrim( &s, p_what );
+}
+
+// in-place trim from both ends of string
+inline std::string & trim( std::string * p_s, const char * p_what = ws )
+{
+    rtrim( p_s, p_what );
+    return ltrim( p_s, p_what );
+}
+
+// copy trim from both ends of string
+inline std::string trim( const std::string & r_s, const char * p_what = ws )
+{
+    std::string s( r_s );
+    return trim( &s, p_what );
+}
+
+}   // namespace clutils
+
+#endif // CLUTIL_STR_OPS
---
+++ test/test-str-ops.cpp
@@ -0,0 +1,103 @@
+//----------------------------------------------------------------------------
+// Copyright (c) 2016, Codalogic Ltd (http://www.codalogic.com)
+// All rights reserved.
+//
+// The license for this file is based on the BSD-3-Clause license
+// (http://www.opensource.org/licenses/BSD-3-Clause).
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions
+// are met:
+//
+// - Redistributions of source code must retain the above copyright notice,
+//   this list of conditions and the following disclaimer.
+// - Redistributions in binary form must reproduce the above copyright notice,
+//   this list of conditions and the following disclaimer in the documentation
+//   and/or other materials provided with the distribution.
+// - Neither the name Codalogic Ltd nor the names of its contributors may be
+//   used to endorse or promote products derived from this software without
+//   specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+// ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+// LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+// CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+// SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+// INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+// CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+// POSSIBILITY OF SUCH DAMAGE.
+//----------------------------------------------------------------------------
+
+#include "cl-utils/str-ops.h"
+
+#include <string>
+
+#include "clunit.h"
+
+using namespace clutils;
+
+TFEATURE( "[lr]?trim - copy" )
+{
+    TTEST( ltrim( "" ) == "" );
+    TTEST( ltrim( "  " ) == "" );
+    TTEST( ltrim( "  x" ) == "x" );
+    TTEST( ltrim( "\r\n \t\v\fxy" ) == "xy" );
+    TTEST( ltrim( "  x  " ) == "x  " );
+
+    TTEST( rtrim( "" ) == "" );
+    TTEST( rtrim( "  " ) == "" );
+    TTEST( rtrim( "x  " ) == "x" );
+    TTEST( rtrim( "xy\r\n \t\v\f" ) == "xy" );
+    TTEST( rtrim( "  x  " ) == "  x" );
+
+    TTEST( trim( "" ) == "" );
+    TTEST( trim( "  " ) == "" );
+    TTEST( trim( "x  " ) == "x" );
+    TTEST( trim( "\r\n \t\v\fxy\r\n \t\v\f" ) == "xy" );
+    TTEST( trim( "  x  " ) == "x" );
+}
+
+void test_in_place_ltrim( const char * in, const char * out )
+{
+    std::string s( in );
+    TTEST( ltrim( &s ) == out );
+    TTEST( s == out );
+}
+
+void test_in_place_rtrim( const char * in, const char * out )
+{
+    std::string s( in );
+    TTEST( rtrim( &s ) == out );
+    TTEST( s == out );
+}
+
+void test_in_place_trim( const char * in, const char * out )
+{
+    std::string s( in );
+    TTEST( trim( &s ) == out );
+    TTEST( s == out );
+}
+
+TFEATURE( "[lr]?trim - in-place" )
+{
+    TCALL( test_in_place_ltrim( "", "" ) );
+    TCALL( test_in_place_ltrim( "  ", "" ) );
+    TCALL( test_in_place_ltrim( "  x", "x" ) );
+    TCALL( test_in_place_ltrim( "\r\n \t\v\fxy", "xy" ) );
+    TCALL( test_in_place_ltrim( "  x  ", "x  " ) );
+
+    TCALL( test_in_place_rtrim( "", "" ) );
+    TCALL( test_in_place_rtrim( "  ", "" ) );
+    TCALL( test_in_place_rtrim( "x  ", "x" ) );
+    TCALL( test_in_place_rtrim( "xy\r\n \t\v\f", "xy" ) );
+    TCALL( test_in_place_rtrim( "  x  ", "  x" ) );
+
+    TCALL( test_in_place_trim( "", "" ) );
+    TCALL( test_in_place_trim( "  ", "" ) );
+    TCALL( test_in_place_trim( "x  ", "x" ) );
+    TCALL( test_in_place_trim( "\r\n \t\v\fxy\r\n \t\v\f", "xy" ) );
+    TCALL( test_in_place_trim( "  x  ", "x" ) );
+}
